46cccb2df51a512b45ed0bb3e2fb0b94c78f0fc5
Anita Kemp and Joel Stockamp <anita@>
2019-08-12 11:12:18 -0700

    add temperature converter

--- js/scripts.js
+++ js/scripts.js
@@ -19,21 +19,20 @@ var divide = function (number1, number2) {
 } ;
 
 
-
-var userMass = parseFloat(prompt("What is your Weight in lbs?"));
-var userHeight = parseFloat(prompt("What is your Heght in inches?"));
-var metricMass = userMass * 0.453592;
-var metricHeight = userHeight * 0.0254;
-
-var bmiResult = (metricMass / (metricHeight * metricHeight));
-
-
+// BMI section
+// var userMass = parseFloat(prompt("What is your Weight in lbs?"));
+// var userHeight = parseFloat(prompt("What is your Height in inches?"));
+// var metricMass = userMass * 0.453592;
+// var metricHeight = userHeight * 0.0254;
+// var bmiResult = (metricMass / (metricHeight * metricHeight));
+// alert(bmiResult);
 
 
+//Temperature Converter
+var userTemp = parseInt(prompt("Enter Farenheit Temperature"));
+var kelvin = (userTemp - 32 ) * 5 / 9 + 273.15;
+var celcius = (userTemp - 32) * 5 / 9;
 
-alert(bmiResult);
+var conversion = "Kelvin:" + kelvin + " Celcius:" + celcius;
 
-
-// var result = divide(number1, number2);
-//
-// alert(result);
+alert(conversion);
