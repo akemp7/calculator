23d40927bf04baa26b96956456e073aa7ef17048
Anita Kemp and Joel Stockamp <anita@>
2019-08-12 10:22:42 -0700

    add add, sub, mult, and div functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,23 @@
 var number1 = parseInt(prompt("Enter first Number"));
 var number2 = parseInt(prompt("Enter second Number"));
 
-var subtract = function (number1, number2) {
 
+var add = function (number1, number2) {
+  return number1 + number2
+};
+
+var subtract = function (number1, number2) {
 	return number1 - number2
 } ;
 
-var result = subtract(number1, number2);
+var multiply = function (number1, number2) {
+	return number1 * number2
+} ;
+
+var divide = function (number1, number2) {
+	return number1 / number2
+} ;
+
+var result = divide(number1, number2);
 
 alert(result);
