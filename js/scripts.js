f4a6b513c9282ab3b1b7ef63c9ab54f5cc58e4b6
Anita Kemp and Joel Stockamp <anita@>
2019-08-12 10:16:48 -0700

    Add html and js documents to make a calculator

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+var number1 = parseInt(prompt("Enter first Number"));
+var number2 = parseInt(prompt("Enter second Number"));
+
+var subtract = function (number1, number2) {
+
+	return number1 - number2
+} ;
+
+var result = subtract(number1, number2);
+
+alert(result);
