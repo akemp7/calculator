7128d867a0898ee16fd780375c68c10996f6631c
Anita Kemp and Joel Stockamp <anita@>
2019-08-12 10:44:16 -0700

    add BMI input and result functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-var number1 = parseInt(prompt("Enter first Number"));
-var number2 = parseInt(prompt("Enter second Number"));
+// var number1 = parseInt(prompt("Enter first Number"));
+// var number2 = parseInt(prompt("Enter second Number"));
 
 
 var add = function (number1, number2) {
@@ -18,6 +18,22 @@ var divide = function (number1, number2) {
 	return number1 / number2
 } ;
 
-var result = divide(number1, number2);
 
-alert(result);
+
+var userMass = parseFloat(prompt("What is your Weight in lbs?"));
+var userHeight = parseFloat(prompt("What is your Heght in inches?"));
+var metricMass = userMass * 0.453592;
+var metricHeight = userHeight * 0.0254;
+
+var bmiResult = (metricMass / (metricHeight * metricHeight));
+
+
+
+
+
+alert(bmiResult);
+
+
+// var result = divide(number1, number2);
+//
+// alert(result);
