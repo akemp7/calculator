6b07b348013309be40a62b43d9e30d7614857ab4
Anita Kemp and Rachael Bouissey <rachael@>
2019-08-14 10:38:49 -0700

    Add front end features

--- calculator/scripts.js
+++ calculator/scripts.js
@@ -18,14 +18,11 @@ var divide = function (number1, number2) {
 
 
 $(document).ready(function(){
-  $("form#add").submit(function(event){
-
-  event.preventDefault();
   var number1 = parseInt($("#add1").val());
   var number2 = parseInt($("#add2").val());
   alert(add(number1, number2));
 
-});
+
 });
 
 
